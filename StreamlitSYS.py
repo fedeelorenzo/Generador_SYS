d61fda471c03402758a2e5b1f0ce51f8a979272a
fedeelorenzo <federico@estudiolorenzoasoc.com>
2025-04-17 14:05:09 -0300

    Update StreamlitSYS.py

--- StreamlitSYS.py
+++ StreamlitSYS.py
@@ -20,7 +20,7 @@ if not st.session_state['auth']:
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("logo.png", width=80)
+    st.image("logo.png", width=180)
 with col2:
     st.title("📊 Generador de Sumas y Saldos")
 
